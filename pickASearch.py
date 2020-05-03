a947f0283c92d9f30f7d68cf0219aabce3aecb64
Jérémy HOLAY <21800092@etu.unicaen.fr>
2020-05-03 14:09:34 +0200

    Random link selection

--- pickASearch.py
+++ pickASearch.py
@@ -1,17 +1,31 @@
 import requests
 import sys
 import json
+import random
 
 if len(sys.argv)!= 2:
     print("Wrong arguments")
     exit(0)
 
 URL = "https://en.wikipedia.org/w/api.php"
-
-params = {"action":"query", "list": "search", "srsearch": sys.argv[1], "format":"json"}
+sroffset="1"
+#First request
+params = {"action":"query", "list": "search", "srsearch": sys.argv[1], "format":"json", "srlimit":"1", "sroffset":sroffset}
 r = requests.get(URL, params= params)
 data = json.loads(r.text)
 query = data['query']
-print(str(query['searchinfo']['totalhits']) + " results")
+
+maxi = query['searchinfo']['totalhits'] -1
+if(maxi > 9999) :
+    maxi = 9999
+sroffset = random.randint(0,maxi)
+
+#Second request
+params = {"action":"query", "list": "search", "srsearch": sys.argv[1], "format":"json", "srlimit":"1", "sroffset":sroffset}
+r = requests.get(URL, params= params)
+data = json.loads(r.text)
+query = data['query']
+
+#Display
 for page in query['search']:
     print (page['title'] + " : https://en.wikipedia.org/wiki/" + page['title'].replace(" ","_"))
